c8fe20853e0a69c70b371d0b1814f450fbbd0784
Yuma Shimizu <e225741@ie.u-ryukyu.ac.jp>
2022-12-12 23:30:19 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。エラー内容を表示します");
+            System.out.println(e.getMessage());
+        }
     }
 
 }
