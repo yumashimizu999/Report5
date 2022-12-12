8f7d7dd3b0e5eb7e7173dd8701cc97c17b69b1e4
Yuma Shimizu <e225741@ie.u-ryukyu.ac.jp>
2022-12-13 00:21:50 +0900

    step3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生しました。エラー内容を表示します");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 
 }
